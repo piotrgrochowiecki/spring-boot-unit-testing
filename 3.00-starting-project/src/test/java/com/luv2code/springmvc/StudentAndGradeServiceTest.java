a557762356d6e85819935c5013185b8d44f035ab
Piotr Grochowiecki <Piotr.Grochowiecki@dxc.com>
2023-07-24 14:11:38 +0200

    addition of service and dao layer and passing test

--- 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -1,7 +1,10 @@
 package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
@@ -11,14 +14,20 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class StudentAndGradeServiceTest {
 
+    @Autowired
+    private StudentAndGradeService studentService;
+
+    @Autowired
+    private StudentDao studentDao;
+
     @Test
     public void createStudentService() {
 
         studentService.createStudent("Chad", "Darby", "chad.darby@test.com");
 
-//        CollegeStudent student = studentDao.findByEmailAddress("chad.darby@test.com");
-//
-//        assertEquals("chad.darby@test.com", student.getEmailAddress(), "find by email");
+        CollegeStudent student = studentDao.findByEmailAddress("chad.darby@test.com");
+
+        assertEquals("chad.darby@test.com", student.getEmailAddress(), "find by email");
     }
 
 }
